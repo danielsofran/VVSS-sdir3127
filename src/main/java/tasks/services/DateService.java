47c67151841de263c32e0f92dd083f551ffd3445
Daniel <92435488+danielsofran@users.noreply.github.com>
2024-04-02 14:43:33 +0300

    Lab2 (#2)
    Co-authored-by: rVaida <rvaida@montran.com>

--- src/main/java/tasks/services/DateService.java
+++ src/main/java/tasks/services/DateService.java
@@ -26,11 +26,11 @@ public class DateService {
         Instant instant = Instant.from(localDate.atStartOfDay(ZoneId.systemDefault()));
         return Date.from(instant);
     }
-    public Date getDateMergedWithTime(String time, Date noTimeDate) {//to retrieve Date object from both DatePicker and time field
+    public static Date getDateMergedWithTime(String time, Date noTimeDate) {//to retrieve Date object from both DatePicker and time field
         String[] units = time.split(":");
         int hour = Integer.parseInt(units[0]);
         int minute = Integer.parseInt(units[1]);
-        if (hour > HOURS_IN_A_DAY || minute > MINUTES_IN_HOUR) throw new IllegalArgumentException("time unit exceeds bounds");
+        if (hour >= HOURS_IN_A_DAY || minute >= MINUTES_IN_HOUR) throw new IllegalArgumentException("time unit exceeds bounds");
         Calendar calendar = GregorianCalendar.getInstance();
         calendar.setTime(noTimeDate);
         calendar.set(Calendar.HOUR_OF_DAY, hour);
