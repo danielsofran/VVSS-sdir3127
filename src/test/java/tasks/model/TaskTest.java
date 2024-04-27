a29ce893b579bf100c6b5d2d9a7d467f01a07e10
Radu Vaida <111689573+raduv7@users.noreply.github.com>
2024-04-27 15:28:30 +0300

    tests for task repo and service

--- src/test/java/tasks/model/TaskTest.java
+++ src/test/java/tasks/model/TaskTest.java
@@ -1,9 +1,6 @@
 package tasks.model;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
--- src/test/java/tasks/model/TasksServiceTest.java
+++ src/test/java/tasks/model/TasksServiceTest.java
@@ -1,4 +1,53 @@
 package tasks.model;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+import tasks.services.TasksService;
+
+import java.util.Date;
+import java.util.stream.StreamSupport;
+
 public class TasksServiceTest {
+
+    @SuppressWarnings("FieldCanBeLocal")
+    private Task task1;
+    @SuppressWarnings("FieldCanBeLocal")
+    private ArrayTaskList repo;
+    private TasksService service;
+
+    @BeforeEach
+    void setUp() {
+        task1 = new Task("ma-sa", new Date(Long.MAX_VALUE / 2));
+        repo = Mockito.spy(ArrayTaskList.class);
+        repo.add(task1);
+        service = new TasksService(repo);
+    }
+
+    @Test
+    void testFilterTasksFullRange() {
+        for (Task filterTask : service.filterTasks(new Date(Long.MIN_VALUE), new Date(Long.MAX_VALUE))) {
+            System.out.println("here we go");
+            System.out.println(filterTask);
+        }
+        Assertions.assertEquals(
+                0,
+                StreamSupport.stream(
+                        service.filterTasks(new Date(Long.MIN_VALUE), new Date(Long.MAX_VALUE)).spliterator(),
+                        false
+                ).count()
+        );
+    }
+
+    @Test
+    void testFilterTasksEmptyRange() {
+        Assertions.assertEquals(
+                0,
+                StreamSupport.stream(
+                        service.filterTasks(new Date(Long.MIN_VALUE), new Date(Long.MIN_VALUE)).spliterator(),
+                        false
+                ).count()
+        );
+    }
 }
